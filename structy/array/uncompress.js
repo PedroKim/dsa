13bf3888b32436472f0f29e12c1e500944ec4446
Taihyung(Peter) Kim <kim.taihyung@gmail.com>
2023-10-02 13:22:02 -0400

    array/uncompress - done

--- structy/array/uncompress.js
+++ structy/array/uncompress.js
@@ -2,8 +2,10 @@
     Uncompress
     Write a function, uncompress, that takes in a string as an argument. The input string 
     will be formatted into multiple groups according to the following pattern:
+    
         <number><char>
         for example, '2c' or '3a'.
+    
     The function should return an uncompressed version of the string where each 'char' of a group
     is well-formed according to the previously mentioned pattern.
 
@@ -16,7 +18,20 @@
 */
 
 const uncompress = (s) => {
-    
+    const digits = '0123456789';
+    let output = "";
+    let i = 0;
+    let j = 0;
+
+    while (j < s.length) {
+        while (digits.includes(s[j])) j += 1;
+        const num = Number(s.slice(i, j));
+        for (let count = 0; count < num; count++) output += s[j];
+        j += 1;
+        i = j;
+    }
+
+    return output;
 };
 
 console.log(uncompress("2c3a1t"));   // -> 'ccaaat'
